bb15c862871fc5541476ddf047ab9ed0e1c680bf
李岱 <352710967@qq.com>
2021-05-15 11:33:11 +0800

    代码优化

--- bmsys-api/bmsys-auth-api/src/main/java/com/ld/bmsys/auth/api/entity/Menu.java
+++ bmsys-api/bmsys-auth-api/src/main/java/com/ld/bmsys/auth/api/entity/Menu.java
@@ -1,6 +1,7 @@
 package com.ld.bmsys.auth.api.entity;
 
 import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
@@ -19,7 +20,7 @@ import java.util.List;
 public class Menu implements Serializable {
 
     private static final long serialVersionUID = 1L;
-
+    @TableId
     @ApiModelProperty(value = "菜单ID")
     private Integer menuId;
 
